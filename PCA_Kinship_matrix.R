f6dff34bde29ae77b9a357c8204e54443e54ac30
Maria Izabel Cavassim Alves <izabelcavassim@gmail.com>
2016-05-26 13:28:44 +0200

    PCA analyses done in R

--- PCA_Kinship_matrix.R
+++ PCA_Kinship_matrix.R
@@ -3,9 +3,9 @@
 #https://tgmstat.wordpress.com/2013/11/28/computing-and-visualizing-pca-in-r/
 #https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html
 
-setwd('C:/Users/MariaIzabel/Desktop/MASTER/PROJECT IN BIOINFORMATICS/prank_alignments_snps/')
+setwd('C:/Users/MariaIzabel/Desktop/MASTER/PROJECTS IN BIOINFORMATICS 2/prank_alignments_snps/')
 require(xlsx)
-strain_names = read.table('strain_ids_hand.txt',sep = '', row.names = NULL, header= FALSE )
+strain_names = read.table('strain_ids.txt',sep = ',', row.names = NULL, header= TRUE )
 origin = read.xlsx('Rhizobium_soiltypes.xlsx', sheetIndex = 1)
 strain_names[,4] = paste(strain_names[,1], strain_names[,2])
 
@@ -16,7 +16,7 @@ library(RcppCNPy)
 
 #Transforming the Kinship matrix in eigen vectors manually and then plot:
 #Eigen vectors
-kinship_matrix = read.table('kinship_matrix.csv', head = F, sep = ',')
+kinship_matrix = read.table('kinship_matrix_all.csv', head = F, sep = ',')
 row.names(kinship_matrix) = strain_names$Mix
 eigenVecs <- eigen(kinship_matrix)$vectors
 lines(x=c(0, eigenVecs[1,1]), y=c(0, eigenVecs[2,1]), col="red")
@@ -35,10 +35,9 @@ finalData <- rowFeatVec %*% rowDataAdj
 plot(t(finalData),  pch = c(16, 17, 18)[as.numeric(strain_names$Country)],  col = c("red", "green","blue")[as.numeric(strain_names$Country)])
 barplot(eigen(kinship_matrix)$values)
 
-require(ggfortify)
 #Using the package prcomp and extract automatically the eigen values and plot:
-
+require(ggfortify)
 pca_prco <- prcomp(kinship_matrix)
-plot(pca_proc$x, pch=seq(9)) #the pca plot
-plot(pca_proc) #the proportion of variance capture by each PC
-autoplot(pca_proc, data = strain_names, colour = 'Country')
+plot(pca_prco$x, pch=seq(9)) #the pca plot
+plot(pca_prco) #the proportion of variance capture by each PC
+autoplot(pca_prco, data = strain_names, colour = 'Country')
